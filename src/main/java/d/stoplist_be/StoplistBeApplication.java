28d90fad82d81e5045a466132c21bc64dd212d4f
misim3 <sim000314@naver.com>
2025-04-19 00:17:33 +0900

    first commit

---
+++ src/main/java/d/stoplist_be/StoplistBeApplication.java
@@ -0,0 +1,13 @@
+package d.stoplist_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoplistBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoplistBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/d/stoplist_be/StoplistBeApplicationTests.java
@@ -0,0 +1,13 @@
+package d.stoplist_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoplistBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
